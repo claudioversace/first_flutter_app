0f27cec15806745a0744612d939ac42712946b32
Kodjo <kodjo@caractereconseil.com>
2020-07-24 19:33:12 +0000

    Box and text property

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,33 @@ class HomePage extends StatelessWidget{
       appBar: AppBar(
         title: Text("My First App"),
       ),
-      body: Container(
-        child: Text("Hi claudio"),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          width: 100,
+          height: 100,
+          alignment: Alignment.topCenter,
+          decoration: BoxDecoration(color: Colors.green,
+          borderRadius: BorderRadius.circular(10),
+          gradient: LinearGradient(colors: [
+            Colors.green,
+            Colors.yellow,
+          ]),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black,
+              blurRadius: 10,
+            )]),
+          child: Text(
+            "I am THOR",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.deepPurple,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
       ),
     );
   }
